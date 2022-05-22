18c9cb9e1ba7da34af06cdc20fc0fa6278ebb5d9
lamodadite <nmb11921@naver.com>
2022-05-23 00:33:07 +0900

    출력을 배열로 하기 전 커밋

--- src/Task07.java
+++ src/Task07.java
@@ -27,12 +27,37 @@ public class Task07 {
         Scanner sc = new Scanner(System.in);
         int numOfLotto = sc.nextInt();
 
+        // 로또 수량만큼의 크기를 가진 이중 배열 선언
+        int[][] lotto = new int[numOfLotto][6];
+
         // 입력한 개수만큼의 로또 개수 생성
         for (int i = 0; i < numOfLotto; i++) {
-            int[] A = lottoMaker();
+            int[] newLotto = lottoMaker();
+            for (int j = 0; j < 6; j++) {
+                lotto[i][j] = newLotto[j];
+            }
         }
 
+        // 출력
+        for (int i = 0; i < numOfLotto; i++) {
+            for (int num : lotto[i]
+            ) {
+                System.out.printf("%02d,", num);
+            }
+            System.out.println();
+        }
         System.out.println();
 
-    }
+        // 로또 당첨 번호
+        int[] winningNum = lottoMaker();
+        System.out.println("[로또 발표]");
+        for (int num :
+                winningNum) {
+            System.out.printf("%02d,", num);
+        }
+
+        // 당첨 번호와 구매 로또 비교하여 숫자 일치 여부 판단
+
+
+        }
 }
