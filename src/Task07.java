eaac3c582b0594a06e95f70ad6ce678bac9c8ef7
lamodadite <nmb11921@naver.com>
2022-05-22 23:25:54 +0900

    일단 하나의 로또 배열을 리턴하는 함수로 만들어보자

--- src/Task07.java
+++ src/Task07.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Task07 {
+
+    public static void lottoMaker (int numOfLotto) {
+        for (int i = 0; i < numOfLotto; i++) {
+        }
+
+
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("[로또 당첨 프로그램]");
+        System.out.print("로또 개수를 입력해 주세요.(숫자 1 ~ 10):");
+
+        // 로또 구매 수량 입력
+        Scanner sc = new Scanner(System.in);
+        int numOfLotto = sc.nextInt();
+
+        // 입력한 개수만큼의 로또 개수 생성
+        lottoMaker(numOfLotto);
+
     }
 }
